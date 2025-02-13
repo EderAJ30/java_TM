e60071b0e604a1313fcbe74b7f79d1aefab97812
EderAJ30 <EAJ30092004@outlook.com>
2025-02-13 11:32:35 -0600

    se sube

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main{
+
+    public static void main(String[] args){
+	System.out.println("App animales");
+	Perro per1= new Perro("Poddle");
+	per1.ladrar();
+
+        System.out.println("Algo nuevo");
+	Pez pez=new Pez();
+	pez.nadar();
+
+   }
+
+}
